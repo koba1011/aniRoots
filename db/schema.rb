eb26dddcf798dfc5c0defe386ce21d684ae59225
kobahumi <72078024+koba1011@users.noreply.github.com>
2021-03-25 16:06:40 +0900

    マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_133142) do
+ActiveRecord::Schema.define(version: 2021_03_25_065818) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "describe", null: false
+    t.text "favorite_scene"
+    t.string "favorite_character"
+    t.integer "spoiler"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
