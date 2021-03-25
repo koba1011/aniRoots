4f9c15243f2862db2a208bda635151724cab4a0a
kobahumi <72078024+koba1011@users.noreply.github.com>
2021-03-25 16:30:16 +0900

    バリデーション 作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :describe
+  end
 end
