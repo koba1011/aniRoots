3bb984124f456db8f70ffa50d708c1cd9cb19426
kobahumi <72078024+koba1011@users.noreply.github.com>
2021-03-29 19:40:05 +0900

    コントローラー設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def edit
+    if @user.id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
